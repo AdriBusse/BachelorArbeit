b7d18008c3c053a21388582a5947c0b4199e213d
AdriBusse <adri.busse@gmail.com>
2022-02-11 10:14:01 +0100

    directives

--- ApolloBackend/src/entity/Sub.ts
+++ ApolloBackend/src/entity/Sub.ts
@@ -1,4 +1,4 @@
-import { ID, ObjectType } from 'type-graphql';
+import { Directive, ID, ObjectType } from 'type-graphql';
 import {
   Entity,
   Column,
@@ -20,6 +20,7 @@ import { Field } from 'type-graphql';
 
 @ObjectType()
 @Entity({ name: 'Subs' })
+@Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
 export class Sub extends BaseEntity {
   constructor(sub: Partial<Sub>) {
     super()
@@ -29,43 +30,53 @@ export class Sub extends BaseEntity {
   @Index()
   @Field(() => ID)
   @PrimaryGeneratedColumn()
+  @Directive("@cacheControl(maxAge: 1000, scope: PUBLIC)")
   id: number;
 
   @Field()
   @Index()
   @Column({ unique: true })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   name: string;
 
   @Field()
   @Column()
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   title: string;
 
   @Field({ nullable: true })
   @Column({ type: 'text', nullable: true })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   describtion: string;
 
   @Field({ nullable: true })
   @Column({ nullable: true })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   imageUrn: string;
 
   @Field({ nullable: true })
   @Column({ nullable: true })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   bannerUrn: string;
 
   @Field()
   @Column()
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   username: string;
 
   @Field()
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   postCount: number;
 
   @Field(() => User)
   @ManyToOne(() => User)
   @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   user: User;
 
   @Field(() => [Post], { nullable: true, })
   @OneToMany(() => Post, (post) => post.sub)
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   posts: Post[];
 
   @Field(() => [User], { nullable: true })
@@ -78,24 +89,29 @@ export class Sub extends BaseEntity {
 
 
   @Field()
+  @Directive("@cacheControl(maxAge: 100, scope: PRIVATE)")
   protected userFollows: number;
 
 
   @Field(() => Number)
+  @Directive("@cacheControl(maxAge: 10, scope: PUBLIC)")
   followerCount: number
 
   //virual field. Not in db
   @Expose()
   @Field()
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   imageUrl: string
 
   //virual field. Not in db
   @Expose()
   @Field(() => String, { nullable: true })
+  @Directive("@cacheControl(maxAge: 100, scope: PUBLIC)")
   bannerUrl: string | null
 
   @Field()
   @CreateDateColumn()
+  @Directive("@cacheControl(maxAge: 1000, scope: PUBLIC)")
   createdAt: Date;
 
   @Field()
